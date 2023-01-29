1a9e886a5501c22ff8f63b63cded1c7a0238719c
Artur <artur.h.0427@gmail.com>
2023-01-29 13:40:51 +0100

    Initialize basic app with backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+
+/* Main */
+class App extends React() {
+	constructor(props) {
+		super(props);
+
+		/* Changeable */
+		this.state = {};
+		
+		/* Static */
+		/* Refs */
+	}
+	
+	componentDidMount() {}
+	componentWillUnmount() {}
+
+	render() {
+		return (
+			<div className="App">
+			</div>
+		);
+	}
+}
+
+/* Exports */
+export default App;
