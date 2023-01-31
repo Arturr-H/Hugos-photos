ef2ea29eda4f242c008dce7703bd1a9a9cad9751
Artur <artur.h.0427@gmail.com>
2023-01-31 21:27:59 +0100

    Create `src/appdata.rs`

---
+++ src/appdata.rs
@@ -0,0 +1,39 @@
+#![allow(dead_code)]
+
+/* Imports */
+use std::collections::HashMap;
+
+/* Global app data */
+struct AppData {
+    collections: HashMap<String, Collection>
+}
+
+/* Image collection */
+struct Collection {
+    images: Vec<Image>,
+    title: String,
+}
+
+/* Image */
+struct Image {
+    date: String,
+    camera: String,
+    place: String,
+
+    pathname: String
+}
+
+/* Method implementations */
+impl AppData {
+    pub fn collections(&self) -> &HashMap<String, Collection> { &self.collections }
+}
+impl Collection {
+    pub fn images(&self) -> &Vec<Image> { &self.images }
+    pub fn title(&self) -> &String { &self.title }
+}
+impl Image {
+    pub fn date(&self) -> &String { &self.date }
+    pub fn camera(&self) -> &String { &self.camera }
+    pub fn place(&self) -> &String { &self.place }
+    pub fn pathname(&self) -> &String { &self.pathname }
+}
