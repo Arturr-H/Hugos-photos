7b43ba3c71a70385aa3d7df6a4404adfb43df380
Artur <artur.h.0427@gmail.com>
2023-01-31 21:47:51 +0100

    Implement save / retrieve methods for `AppData` and more

--- src/appdata.rs
+++ src/appdata.rs
@@ -1,20 +1,28 @@
 #![allow(dead_code)]
 
 /* Imports */
-use std::collections::HashMap;
+use std::{ collections::HashMap, fs::File, io::{Read, Write} };
+use bincode;
+use serde_derive::{ Deserialize, Serialize };
+
+/* Constants */
+const PATH_TO_APPDATA_SAVE:&'static str = "./appdata";
 
 /* Global app data */
+#[derive(Deserialize, Serialize)]
 pub struct AppData {
     collections: HashMap<String, Collection>
 }
 
 /* Image collection */
+#[derive(Deserialize, Serialize)]
 pub struct Collection {
     images: Vec<Image>,
     title: String,
 }
 
 /* Image */
+#[derive(Deserialize, Serialize)]
 pub struct Image {
     date: String,
     camera: String,
@@ -26,6 +34,20 @@ pub struct Image {
 /* Method implementations */
 impl AppData {
     pub fn collections(&self) -> &HashMap<String, Collection> { &self.collections }
+    pub fn new() -> Self { Self { collections: HashMap::new() } }
+
+    /* Save / retrieve */
+    pub fn from_file() -> Self {
+        let mut file = File::open(PATH_TO_APPDATA_SAVE).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+        bincode::deserialize(&buffer).unwrap()
+    }
+    pub fn save(&self) -> () {
+        let mut file = File::create(PATH_TO_APPDATA_SAVE).unwrap();
+        let buffer = bincode::serialize(&self).unwrap();
+        file.write_all(&buffer).unwrap();
+    }
 }
 impl Collection {
     pub fn images(&self) -> &Vec<Image> { &self.images }
