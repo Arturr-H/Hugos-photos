c9091b32bdc6916404d9cc358751d6e54ec72243
Artur <artur.h.0427@gmail.com>
2023-02-08 21:25:07 +0100

    Remove unused darkmode button

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -17,17 +17,10 @@ export default class Navbar extends React.PureComponent {
         ];
     }
 
-    toggleDarkmode = () => {
-        let toggled = !this.state.darkMode;
-        this.setState({ darkMode: toggled });
-        this.props.onToggleDarkmode(toggled);
-    }
-
     render() {
         return (
             <nav>
-                {this.props.canToggleDarkmode ? <img alt="darkmode" onClick={this.toggleDarkmode} className="darkmode-button" size={68} src={this.staticSvgs[this.state.darkMode ? 1 : 0]} /> : null }
-                {this.props.canToggleDarkmode ? <a className="link" href="/about">About Me</a> : null }
+                {this.props.aboutMeVisible ? <a className="link" href="/about">About Me</a> : null }
                 {/* <a className="link" href="/contact">Kontakt</a>
                 <a className="link" href="/help">Hjälp</a> */}
             </nav>
