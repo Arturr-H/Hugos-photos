b50487c9bd94fb8d166c699946ad5a2d9af29923
Artur <artur.h.0427@gmail.com>
2023-02-12 22:10:39 +0100

    Create token auth via `AUTH.txt file`

--- frontend/src/post/Post.js
+++ frontend/src/post/Post.js
@@ -1,5 +1,7 @@
 /* Imports */
 import React from "react";
+import Globals from "../Globals";
+import axios from "axios";
 
 /* Main */
 export default class Post extends React.PureComponent {
@@ -7,11 +9,62 @@ export default class Post extends React.PureComponent {
         super(props);
     }
 
+    componentDidMount() {
+        window.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        }, false);
+        window.addEventListener("drop",function(e){
+            e.preventDefault();
+        }, false);
+    }
+
+    onEnter = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+    onDrop = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            let selectedFile = e.dataTransfer.files[0];
+            const formData = new FormData();
+        
+            formData.append(
+                "image",
+                selectedFile,
+                selectedFile.name
+            );
+            
+            axios.post(Globals.backendUrl + "check-auth", formData, {}).then(e => {
+                if (e.data.status == 200) {
+                    console.log(e.data);
+                    this.setCookie("token", e.data.token, 7);
+                }
+                alert(e.data.message);
+            });
+        }
+    }
+
+    setCookie = (cname, cvalue, exdays) => {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
     render() {
         return (
             <div className="post">
-                <a target="_blank" href="/create"><button>Skapa kollektion</button></a>
-                <a target="_blank" href="/add"><button>Lägg till i kollektion</button></a>
+                <div className="row">
+                    <a target="_blank" href="/create"><button>Skapa kollektion</button></a>
+                    <a target="_blank" href="/add"><button>Lägg till i kollektion</button></a>
+                </div>
+                <form className="row" onDrop={this.onDrop} onDragEnter={this.onEnter} onSubmit={(e) => e.preventDefault()}>
+                <input type="file" id="input-file-upload" multiple={false} />
+                    <div className="upload">
+                        <h1>Upload</h1>
+                    </div>
+                </form>
             </div>
         )
     }
