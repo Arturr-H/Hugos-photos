28391a1a12ecc50b562698c1ab892f0df303642c
Artur <artur.h.0427@gmail.com>
2023-06-16 23:57:42 +0200

    Remove unused code and add `alt` tag to img

--- frontend/src/ScaledImage.js
+++ frontend/src/ScaledImage.js
@@ -3,17 +3,12 @@ import React  from "react";
 
 /* Main */
 export default class ScaledImage extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {}
-
     render() {
         return (
             <img
                 src={this.props.source}
                 className={"scaled-image " + this.props.className}
+                alt="scaled image which is showed upon clicking an image"
                 style={{
                     filter: "blur(" + this.props.blur + "px) brightness(" + this.props.brightness + "%)",
                 }}
